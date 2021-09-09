1743602fc0d32ab3bef1aa8e8283c851b7494dac
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-09 15:03:27 +0530

    Added: code for creating linked list

---
+++ src/com/list/LinkedList.java
@@ -0,0 +1,43 @@
+package com.list;
+
+public class LinkedList {
+	
+	private Node head;
+
+	private class Node {
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+			next = null;
+		}
+	}
+	
+	public boolean add(final int data) {
+		boolean isAdded = false;
+		Node newNode = new Node(data);
+		if(head == null) {
+			head = newNode;
+			isAdded = true;
+		}
+		else {
+			Node temp = head;
+			while(temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = newNode;
+			isAdded = true;
+		}
+		return isAdded;
+	}
+	
+	public void print() {
+		Node temp = head;
+		System.out.println("Linked list is");
+		while(temp != null) {
+			System.out.print(temp.data+"->");
+			temp = temp.next;
+		}
+	}
+}
--- src/com/list/Main.java
+++ src/com/list/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to linked list program");
+		LinkedList list = new LinkedList();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		list.print();
 	}
 
 }
